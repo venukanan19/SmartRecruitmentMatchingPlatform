95a7befad907ac010ebc9bd3705d02e95d0eb8a5
Kunasingam Thusa <UT011663@unicomtic.com>
2026-08-05 16:40:02 +0530

    jobseeker: add job seeker entity models

---
+++ SmartRecruitment.API/Models/Entities/CvMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartRecruitment.API.Models.Entities
+{
+    public class CvMetadata
+    {
+        public int CvMetadataId { get; set; }
+
+        public int JobSeekerProfileId { get; set; }
+
+        public string OriginalFileName { get; set; } = string.Empty; 
+
+        public long FileSize { get; set; }
+
+        public DateTime UploadedAt { get; set; }
+
+        public JobSeekerProfile JobSeekerProfile { get; set; } = null!;
+    }
+}
---
+++ SmartRecruitment.API/Models/Entities/JobSeekerSkill.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartRecruitment.API.Models.Entities
+{
+    public class JobSeekerSkill
+    {
+        public int JobSeekerProfileId { get; set; }
+
+        public int SkillId { get; set; }
+
+        // New fields
+
+        // Value between 1 and 5
+        public int ProficiencyLevel { get; set; }
+
+        // Number of years of experience
+        public int YearsOfExperience { get; set; }
+
+        // Navigation properties
+
+        public JobSeekerProfile JobSeekerProfile { get; set; } = null!;
+
+      //  public Skill Skill { get; set; } = null!;
+    }
+}
