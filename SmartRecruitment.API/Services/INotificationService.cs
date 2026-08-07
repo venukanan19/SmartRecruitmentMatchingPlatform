0af08d6654891a2e893d34ed1e23b45452a89436
georgehentry3 <georgehentry3@gmail.com>
2026-08-07 10:07:50 +0530

    correct contactRequest.cs & some files

--- SmartRecruitment.API/Services/INotificationService.cs
+++ SmartRecruitment.API/Services/INotificationService.cs
@@ -1,6 +1,15 @@
-﻿namespace SmartRecruitment.API.Services
+﻿using SmartRecruitment.API.Models.DTOs.Notification;
+
+namespace SmartRecruitment.API.Services
 {
-    public class INotificationService
+    public interface INotificationService
     {
+        Task<IEnumerable<NotificationResponseDto>> GetUserNotificationsAsync(int userId);
+
+        Task<NotificationResponseDto?> GetNotificationByIdAsync(int notificationId);
+
+        Task<bool> MarkAsReadAsync(int notificationId);
+
+        Task<bool> DeleteNotificationAsync(int notificationId);
     }
 }
