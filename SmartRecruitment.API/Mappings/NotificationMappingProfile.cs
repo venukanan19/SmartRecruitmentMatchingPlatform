8c02c19a0c3fd615adfd0c3f1771f4967ad1ddf8
georgehentry3 <georgehentry3@gmail.com>
2026-08-08 09:23:52 +0530

    last file pushing

--- SmartRecruitment.API/Mappings/NotificationMappingProfile.cs
+++ SmartRecruitment.API/Mappings/NotificationMappingProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace SmartRecruitment.API.Mappings
-{
-    public class NotificationMappingProfile
-    {
-    }
-}
+﻿//using AutoMapper;
+//using SmartRecruitment.API.Models.DTOs.Notification;
+//using SmartRecruitment.API.Models.Entities;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//namespace SmartRecruitment.API.Mappings
+//{
+//    public class NotificationMappingProfile : Profile
+//    {
+//        public NotificationMappingProfile()
+//        {
+//            CreateMap<Notification, NotificationResponseDto>();
+//        }
+//    }
+//}
